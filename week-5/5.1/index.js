7e6a5ecb8143c2dccbdd72ab44e168f66e190c8c
Deep Detroja <deepdetroja1223@gmail.com>
2025-10-22 20:51:33 +0530

    week-5.1

---
+++ week-5/5.1/index.js
@@ -0,0 +1,54 @@
+// Question : # Create an HTTP Server
+
+// It should have 4 routes
+
+// 1. http://localhost:3000/multiply?a=1&b=2
+// 2. http://localhost:3000/sum?a=1&b=2
+// 3. http://localhost:3000/divide?a=1&b=2
+// 4. http://localhost:3000/subtract?a=1&b=2
+
+// Inputs given at the end after `?` are known as query parameters (usually used in GET requests)
+// The way to get them in an HTTP route is by extracting them from the `req` argument (req.query.a , req.query.b)
+
+
+const express = require("express");
+const app = express();
+
+app.get("/multiply", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    res.json({
+        ans : a*b
+    })
+
+};
+app.get("/sum", function(req, res) {
+    const a = paresInt(req.query.a);
+    const b = paresInt(req.query.b);
+
+    res.json({
+        ans : a+b
+    })
+
+};
+app.get("/divide", function(req, res) {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans : a/b
+    })
+
+};
+app.get("/substract", function(req, res) {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    res.json({
+        ans : a-b
+    })
+
+};
+
+app.listen(3000);
