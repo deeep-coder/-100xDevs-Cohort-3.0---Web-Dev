f4e01f8d112f9ee8cbd118ecb86ea8f2b41c75e8
Deep Detroja <deepdetroja1223@gmail.com>
2025-11-21 10:51:14 +0530

    assignment done

---
+++ week-4_offline/4.2(Offline)/Assignment/01.requestCounter.js
@@ -0,0 +1,33 @@
+/*
+ You have to create a middleware for logging the number of requests on a server
+
+You have been given an express server which has a few endpoints.
+
+Your task is to create a global middleware (app.use) which will maintain a count of the number of 
+requests made to the server in the global requestCount variable
+
+*/
+
+import express from 'express'
+
+const app = express();
+let requestCount = 0;
+
+app.use((req, res, next) => {
+    requestCount = requestCount + 1;
+    next();
+})
+
+app.get('/user', function (req, res) {
+    res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function (req, res) {
+    res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount', function (req, res) {
+    res.status(200).json({ requestCount });
+});
+
+app.listen(3000);
